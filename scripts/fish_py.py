c1897d09964a342457e9333032ed2d2bc6be8e91
Adam Streck <adam.streck@gmail.com>
2022-01-28 15:19:48 +0100

    The plot with interpolation is finished.

--- scripts/fish_py.py
+++ scripts/fish_py.py
@@ -3,7 +3,6 @@
 import argparse
 import random
 import sys
-import math
 import logging
 
 import numpy as np
@@ -14,7 +13,7 @@ from matplotlib import cm
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 eps = sys.float_info.epsilon
-random.seed(3)
+
 
 # Taken from https://stackoverflow.com/questions/3160699/python-progress-bar
 def progressbar(it, prefix="", size=60, file=sys.stdout):
@@ -33,11 +32,10 @@ def progressbar(it, prefix="", size=60, file=sys.stdout):
     file.flush()
 
 
-def build_tree(par_df, ids, rootId):
-    tree = {cloneId: list(children) for cloneId in ids if
-            len(children := par_df.loc[par_df["ParentId"] == cloneId, "ChildId"]) > 0}
+def build_tree(par_df, ids, root_id):
+    tree = {cid: list(children) for cid in ids if len(children := par_df.loc[par_df["ParentId"] == cid, "ChildId"]) > 0}
     if -1 not in tree:
-        tree[-1] = [rootId]
+        tree[-1] = [root_id]
     return tree
 
 
@@ -45,28 +43,28 @@ def line_from_data(px_row_orig, width):
     result = []
     pop_total = sum(row_it[0] for row_it in px_row_orig)
     px_row = [[px_row_orig[i][0] * width / pop_total, px_row_orig[i][1]] for i in range(len(px_row_orig))]
-    contr = 1.0
+    contribution = 1.0
     index = 0
     new_px = np.zeros(4)
     while index < len(px_row):
         if px_row[index][0] >= eps:
-            factor = contr if px_row[index][0] >= contr else px_row[index][0]
+            factor = contribution if px_row[index][0] >= contribution else px_row[index][0]
             px_row[index][0] -= factor
-            contr -= factor
+            contribution -= factor
             new_px += px_row[index][1] * factor
-            if contr < eps:
-                contr = 1.0
+            if contribution < eps:
+                contribution = 1.0
                 result.append(new_px)
                 new_px = np.zeros(4)
         else:
             index += 1
     if len(result) < width:
-        result.append(new_px + contr * np.ones(4) * .5)  # Add gray to the last pixel if needed
+        result.append(new_px + contribution * np.ones(4) * .5)  # Add gray to the last pixel if needed
     return result
 
 
-# Interpolates between generations
-def get_count_at_gen(pops_df, first_step, last_step, step, clone_id):
+# Interpolates between steps
+def get_count_at_step(pops_df, first_step, last_step, step, clone_id):
     cp_df = pops_df[pops_df["Id"] == clone_id]
     match = cp_df[cp_df["Step"] == step]
     if not match.empty:
@@ -75,29 +73,28 @@ def get_count_at_gen(pops_df, first_step, last_step, step, clone_id):
     l_neigh_i = cp_df[cp_df["Step"] < step]["Step"]
     u_neigh_i = cp_df[cp_df["Step"] > step]["Step"]
 
-    if l_neigh_i.size > 0 and u_neigh_i.size > 0:
+    if l_neigh_i.size > 0:
         ind_min = l_neigh_i.idxmax()
-        ind_max = u_neigh_i.idxmin()
-        prev_gen, prev_pop = cp_df.loc[ind_min, ["Step", "Pop"]].tolist()
-        next_gen, next_pop = cp_df.loc[ind_max, ["Step", "Pop"]].tolist()
-    elif l_neigh_i.size > 0:
-        ind_min = l_neigh_i.idxmax()
-        prev_gen, prev_pop = cp_df.loc[ind_min, ["Step", "Pop"]].tolist()
-        next_gen, next_pop = last_step, 0
-    elif u_neigh_i.size > 0:
-        ind_max = u_neigh_i.idxmin()
-        prev_gen, prev_pop = first_step, 0
-        next_gen, next_pop = cp_df.loc[ind_max, ["Step", "Pop"]].tolist()
+        prev_step, prev_pop = cp_df.loc[ind_min, ["Step", "Pop"]].tolist()
     else:
+        prev_step, prev_pop = first_step, 0
+
+    if u_neigh_i.size > 0:
+        ind_max = u_neigh_i.idxmin()
+        next_step, next_pop = cp_df.loc[ind_max, ["Step", "Pop"]].tolist()
+    else:
+        next_step, next_pop = last_step, 0
+
+    if prev_pop + next_pop == 0:
         return 0
 
-    pop = ((step - prev_gen) * next_pop + (next_gen - step) * prev_pop) / (next_gen - prev_gen)
+    pop = ((step - prev_step) * next_pop + (next_step - step) * prev_pop) / (next_step - prev_step)
     log.debug(f"Id: {clone_id}, Step: {step}, Pop: {pop}")
     return pop
 
 
-def get_pop_dict(pops_df, ids, fg, lg, gen, max_pop, normalize):
-    pop_dict = { cloneId: count for cloneId in ids if (count := get_count_at_gen(pops_df, fg, lg, gen, cloneId)) > 0 }
+def get_pop_dict(pops_df, ids, fg, lg, step, max_pop, normalize):
+    pop_dict = {clone_id: count for clone_id in ids if (count := get_count_at_step(pops_df, fg, lg, step, clone_id)) > 0}
     if not normalize:
         pop_count = sum(pop_dict.values())
         remainder = max_pop - pop_count
@@ -131,7 +128,7 @@ def pixels_to_img(img_pixels, res):
 
 def get_image_pixels(tree, ids, root_id, pop_df, col_map, max_pop, breath, first_step, last_step, normalize=True):
     img_pixels = []
-    for step in progressbar(range(first_step, last_step), "Step: "):
+    for step in progressbar(range(first_step, last_step + 1), "Step: "):
         pop_dict = get_pop_dict(pop_df, ids, first_step, last_step, step, max_pop, normalize)
         if len(pop_dict) > 0:
             id_rows = rec_descend(tree, pop_dict, root_id if normalize else -1)
@@ -144,7 +141,7 @@ def get_image_pixels(tree, ids, root_id, pop_df, col_map, max_pop, breath, first
 
 
 def plot_fish(pop_df, par_df, first_step, last_step, res):
-    max_pop = pop_df[["Gen", "Pop"]].groupby("Gen", as_index=False).sum().max()
+    max_pop = pop_df[["Step", "Pop"]].groupby("Step", as_index=False).sum().max()
 
     parents = par_df["ParentId"].unique()
     children = par_df["ChildId"].unique()
@@ -168,34 +165,39 @@ def plot_fish(pop_df, par_df, first_step, last_step, res):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create a Fish (Muller) plot for the given evolutionary tree.')
-    parser.add_argument("populations", type=str, help="A CSV file with the header \"Id,Gen,Pop\".")
+    parser.add_argument("populations", type=str, help="A CSV file with the header \"Id,Step,Pop\".")
     parser.add_argument("parent_tree", type=str, help="A CSV file with the header \"ParentId,ChildId\".")
     parser.add_argument("output", type=str, help="Output image filepath. The format must support alpha channels.")
-    parser.add_argument("-F", "--first", dest="first_gen", type=int, help="The generation to start plotting from.")
-    parser.add_argument("-L", "--last", dest="last_gen", type=int, help="The generation to end the plotting at.")
+    parser.add_argument("-F", "--first", dest="first_step", type=int, help="The step to start plotting from.")
+    parser.add_argument("-L", "--last", dest="last_step", type=int, help="The step to end the plotting at.")
     parser.add_argument("-W", "--width", dest="width", type=int, help="Output image width", default=1280)
     parser.add_argument("-H", "--height", dest="height", type=int, help="Output image height", default=720)
+    randint = random.randint(0, sys.maxsize)
+    parser.add_argument("-S", "--seed", dest="seed", type=int, help="Seed for colors", default=randint)
+    parser.add_argument('-I', dest="interpolate", default=True, help='Turn off interpolation', action='store_false')
+
     args = parser.parse_args()
 
     resolution = [args.width, args.height]
+    random.seed(args.seed)
+    print(f"Creating {args.width}*{args.height} fish plot file {args.output} with the seed {args.seed}")
 
-    populations_df = pd.read_csv(args.populations)
+    pops_df = pd.read_csv(args.populations)
     parent_df = pd.read_csv(args.parent_tree)
 
-    min_gen = populations_df["Gen"].min()
-    first_gen = max(min_gen, args.first_gen) if args.first_gen else min_gen
+    min_step = pops_df["Step"].min()
+    fs = max(min_step, args.first_step) if args.first_step else min_step
 
-    max_gen = populations_df["Gen"].max()
-    last_gen = min(args.last_gen, max_gen) if args.last_gen else max_gen
+    max_step = pops_df["Step"].max()
+    ls = min(args.last_step, max_step) if args.last_step else max_step
 
-    if first_gen >= last_gen:
-        err = f"First generation {first_gen} must be before the last generation {last_gen}."
+    if fs >= ls:
+        err = f"First step {fs} must be before the last step {ls}."
         raise Exception(err)
 
-    gen_count = max_gen - min_gen
-    populations_df["Step"] = populations_df["Gen"] * resolution[0] / gen_count
-    first_step = int(populations_df["Step"].min())
-    last_step = int(populations_df["Step"].max())
+    if args.interpolate:
+        step_count = max_step - min_step + 1
+        pops_df["Step"] = pops_df["Step"] * resolution[0] / step_count
 
-    img = plot_fish(populations_df, parent_df, first_step, last_step, resolution)
+    img = plot_fish(pops_df, parent_df, int(pops_df["Step"].min()), int(pops_df["Step"].max()), resolution)
     img.save(args.output)
