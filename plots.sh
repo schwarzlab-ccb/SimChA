33a0a0aafacf13c5586585bf76c380f7960fd5f4
Tom Kaufmann <tkau93@gmail.com>
2022-02-02 20:27:47 +0100

    New fish plot

--- plots.sh
+++ plots.sh
@@ -1,7 +1,8 @@
 echo "Plotting Parent Graph"
 dot -Tpng out/parent_graph.dot > out/parent_tree.png
 echo "Plotting Fish Plot"
-python3 scripts/fish.py out/populations.csv out/parent_tree.csv out/fish.png -W 600 -H 400 
+python3 scripts/fish.py out/populations.csv out/parent_tree.csv out/fish.png -S 42
+python3 scripts/fish.py out/populations.csv out/parent_tree.csv out/fish_abs.png -S 42 --absolute
 echo "Plotting CN Tracks"
 python3 scripts/plot_cn_tracks.py --input_folder out --output_folder out --fraction
 echo "Plotting Population Dynamics"
