0e02ee4435e3feae6662941734d8c504d40be107
Adam Streck <adam.streck@gmail.com>
2022-03-24 12:42:14 +0100

    plotting with filtered.

--- plots.sh
+++ plots.sh
@@ -9,9 +9,9 @@ pyfish $out/populations.csv $out/parent_tree.csv $out/fish.png -R $col -S $smoot
 pyfish $out/populations.csv $out/parent_tree.csv $out/fish_abs.png -R $col -S $smooth -a  
 echo "Plotting Parent Graph"
 dot -Tpng $out/parent_graph.dot > $out/parent_tree.png
-echo "Plotting Population Dynamics"
-python3 scripts/plot_population_dynamics.py --input_folder $out --output_folder $out
-#echo "plotting Metrics Overview"
-#python3 scripts/plot_metrics_single_experiment.py --input_folder $out
+#echo "Plotting Population Dynamics"
+#python3 scripts/plot_population_dynamics.py --input_folder $out --output_folder $out
+echo "plotting Metrics Overview"
+python3 scripts/plot_metrics_single_experiment.py --input_folder $out
 #echo "Plotting CN Tracks"
 #python3 scripts/plot_cn_tracks.py --input_folder out --output_folder out --fraction
