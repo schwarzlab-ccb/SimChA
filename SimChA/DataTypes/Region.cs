07b93bad53d3cc0e5a80cf0769e0362083c5f8a1
Adam_Streck <adam.streck@gmail.com>
2022-12-09 12:11:50 +0100

    Refactoring the simulation function.

--- SimChA/DataTypes/Region.cs
+++ SimChA/DataTypes/Region.cs
@@ -6,7 +6,7 @@ public struct Region
 {
     public ChromID ChromId;
     public int Start; // Zero-index of first base
-    public int End; // Zero-index one beding the last base
+    public int End; // Zero-index one behind the last base
     public bool Forward; // True if the region is placed in the forward direction, false otherwises
 
     public int Length => End - Start;
--- SimChA/Simulation/Simulator.cs
+++ SimChA/Simulation/Simulator.cs
@@ -7,7 +7,8 @@ namespace SimChA.Simulation;
 
 public static class Simulator
 {
-    public static void AssignMutationsRecursive(Clone currentClone, List<Clone> clones, AberrationsInfo aberrationsInfo, Random rnd)
+    public static void AssignMutationsRecursive(Clone currentClone, List<Clone> clones, AberrationsInfo aberrationsInfo,
+        Random rnd)
     {
         foreach (int cloneId in currentClone.ChildrenIDs)
         {
@@ -15,10 +16,13 @@ public static class Simulator
             for (int i = 0; i < clones[cloneId].MutCount; i++)
             {
                 var aberration = aberrationsInfo.PickRandomMutation(rnd);
-                string region = clones[cloneId].Karyotype.ApplyAberration(rnd, aberration, aberrationsInfo.Map[aberration], clones[cloneId].Name);
+                string region = clones[cloneId].Karyotype.ApplyAberration(rnd, aberration,
+                    aberrationsInfo.Map[aberration], clones[cloneId].Name);
                 AssignFitness(clones[cloneId]);
-                new Abberation(clones[cloneId].Name, aberration.ToString(), region, (float) clones[cloneId].deltaFitness);
+                new Abberation(clones[cloneId].Name, aberration.ToString(), region,
+                    (float) clones[cloneId].deltaFitness);
             }
+
             AssignMutationsRecursive(clones[cloneId], clones, aberrationsInfo, rnd);
         }
     }
@@ -31,52 +35,51 @@ public static class Simulator
         return new List<Clone> {parent, child};
     }
 
+    // Represents the limitation of space in the nucleus - more chromosomes ==> more stress
+    // TODO: This needs to be validated
+    private static float CalcStress(float stressFactor, int chromCount)
+        => stressFactor * (float) Math.Pow(Math.Max(0, chromCount - 46), 2);
+
     public static void AssignFitness(Clone clone)
     {
         clone.deltaFitness = 0;
-        float stress = 0;
-        float essentialityFitness = 0;
         float tsgOgFitness = 0;
-        if(clone.Karyotype.ChromCount > 46)
+        float stress = CalcStress(0.0001f, clone.Karyotype.ChromCount);
+
+        var essentialStillThere = new List<Gen>();
+        var tsgOgStillThere = new List<Gen>();
+        foreach (var chr in clone.Karyotype.Chromosomes)
         {
-            stress = (float)(-0.0001 * Math.Pow(clone.Karyotype.ChromCount-46, 2));
-        }
-        List<Gen> essentialStillThere = new List<Gen>();
-        List<Gen> tsgOgStillThere = new List<Gen>();
-        foreach(Chromosome chr in clone.Karyotype.Chromosomes)
-        {
-            for(int i = 0; i < chr._regions.Count(); i++)
+            foreach (var region in chr._regions)
             {
-
-                essentialStillThere.AddRange(GenList.EssentialList[chr._regions[i].ChromId.ChromNum].FindAll(x => x.start > chr._regions[i].Start && x.stop < chr._regions[i].End));
-                tsgOgStillThere.AddRange(GenList.TsgOgList[chr._regions[i].ChromId.ChromNum].FindAll(x => x.start > chr._regions[i].Start && x.stop < chr._regions[i].End));
+                var chromNum = region.ChromId.ChromNum;
+                essentialStillThere.AddRange(GenList.EssentialList[chromNum]
+                    .FindAll(x => x.start > region.Start && x.stop < region.End));
+                tsgOgStillThere.AddRange(GenList.TsgOgList[chromNum]
+                    .FindAll(x => x.start > region.Start && x.stop < region.End));
             }
-        }     
-
-        List<Gen> essentialsLost = FindMissingGenes(essentialStillThere, GenList.EssentialList);
-        List<Gen> tsgOgLost = FindMissingGenes(tsgOgStillThere, GenList.TsgOgList);
-        tsgOgLost.AddRange(tsgOgStillThere.GroupBy(x => x).Where(g => g.Count() < 2).Select(g => g.Key));
-        List<Gen> tsgOgAdded = new List<Gen>();
-        tsgOgAdded.AddRange(tsgOgStillThere.GroupBy(x => x).Where(g => g.Count() > 2).Select(global => global.Key));
-        foreach(var essential in essentialsLost)
-        {
-            essentialityFitness += essential.deltaFitness;
         }
-        foreach(var tsgOg in tsgOgAdded)
+
+        var essentialsLost = FindMissingGenes(essentialStillThere, GenList.EssentialList);
+        var tsgOgLost = FindMissingGenes(tsgOgStillThere, GenList.TsgOgList)
+                .Concat(tsgOgStillThere.GroupBy(x => x).Where(g => g.Count() < 2).Select(g => g.Key));
+        var tsgOgAdded = tsgOgStillThere.GroupBy(x => x).Where(g => g.Count() > 2).Select(g => g.Key);
+        float essentialityFitness = essentialsLost.Sum(essential => essential.deltaFitness);
+        foreach (var tsgOg in tsgOgAdded)
         {
             tsgOgFitness -= tsgOg.deltaFitness;
         }
-        foreach(var tsgOg in tsgOgLost)
+        foreach (var tsgOg in tsgOgLost)
         {
             tsgOgFitness += tsgOg.deltaFitness;
         }
-        
+
         clone.deltaFitness = stress + tsgOgFitness + essentialityFitness;
     }
 
     public static bool AddIfNotThere<ChromNum, Gen>(this Dictionary<ChromNum, List<Gen>> dict, ChromNum key, Gen value)
     {
-        if(dict.ContainsKey(key))
+        if (dict.ContainsKey(key))
         {
             dict[key].Add(value);
         }
@@ -84,15 +87,18 @@ public static class Simulator
         {
             dict.Add(key, new List<Gen> {value});
         }
+
         return true;
     }
 
     private static List<Gen> FindMissingGenes(List<Gen> genes, Dictionary<ChromNum, List<Gen>> dict)
     {
         List<Gen> missingGenes = new List<Gen>();
-        foreach(var list in dict){
+        foreach (var list in dict)
+        {
             missingGenes.AddRange(list.Value.Except(genes));
         }
+
         return missingGenes;
     }
 }
