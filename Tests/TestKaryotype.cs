d45f3c9eca79512e89d0580d038b4927318428d8
Felix Schifferdecker <felix.schifferdecker@gmx.de>
2023-01-11 18:39:18 +0100

    Test for getPresentGenes

--- Tests/TestKaryotype.cs
+++ Tests/TestKaryotype.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using SimChA.DataTypes;
 using SimChA.IO;
@@ -23,21 +24,21 @@ public class TestKaryotype
     public void TestDeletion()
     {
         _kar.ApplyAberration(_rnd, AberrationEnum.ChromDeletion, new BaseAbbP(1f));
-        Assert.AreEqual(45, _kar.ChrCount);
+        Assert.AreEqual(45, _kar.ContigCount);
     }
     
     [Test]
     public void TestDuplication()
     {
         _kar.ApplyAberration(_rnd, AberrationEnum.ChromDuplication, new BaseAbbP(1f));
-        Assert.AreEqual(47, _kar.ChrCount);
+        Assert.AreEqual(47, _kar.ContigCount);
     }
     
     [Test]
     public void TestBFB()
     {
         _kar.ApplyAberration(_rnd, AberrationEnum.BreakageFusionBridge, new FractionAbbP(1f, .1f));
-        Assert.AreEqual(46, _kar.ChrCount);
+        Assert.AreEqual(46, _kar.ContigCount);
     }
     
     [Test]
@@ -48,5 +49,23 @@ public class TestKaryotype
             _kar.ApplyAberration(_rnd, AberrationEnum.ChromDeletion, new BaseAbbP(1f));
         }
         Assert.AreEqual("[]", _kar.ToString());
+    }
+
+    [Test]
+    public void TestGetPresentGenes()
+    {
+        _kar = new Karyotype(false);
+        var tsgOgLists = new Dictionary<ChrNo, List<Gene>>();
+        foreach(var chrNo in (ChrNo[]) Enum.GetValues(typeof(ChrNo)))
+        {
+            tsgOgLists.Add(chrNo, new List<Gene>{new Gene("T" + chrNo.ToString(), 
+                new Region(0, 50, new ChrID(chrNo, false)), (float) _rnd.NextDouble())});
+        }
+        var tsgOgsPresent = _kar.GetPresentGenes(tsgOgLists);
+        Assert.AreEqual(_kar.ContigCount, tsgOgsPresent.Count);
+        _kar.ApplyAberration(_rnd, AberrationEnum.ChromDeletion, new BaseAbbP(1f));
+        _kar.ApplyAberration(_rnd, AberrationEnum.ChromDeletion, new BaseAbbP(1f));
+        tsgOgsPresent = _kar.GetPresentGenes(tsgOgLists);
+        Assert.AreEqual(_kar.ContigCount, tsgOgsPresent.Count);
     }
 }
