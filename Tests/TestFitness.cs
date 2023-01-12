3d993b65d8c6e3a8bcb6116e00aa241bbce5804f
Felix Schifferdecker <felix.schifferdecker@gmx.de>
2023-01-12 16:30:23 +0100

    Some tests for Fitness

--- Tests/TestFitness.cs
+++ Tests/TestFitness.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using SimChA.Computation;
 using SimChA.DataTypes;
+using SimChA.Simulation;
 
 namespace Tests;
 
@@ -34,13 +35,32 @@ public class TestFitness
     [Test]
     public void TestTsgOgTerm()
     {
-        // TODO: @Felix
+        Assert.AreEqual(0, Fitness.TsgOgTerm(new List<(Gene, int)>()));
+        
+        var testNoEffect = new List<(Gene, int)> {(MakeGene(ChrNo.chr1, 0), 0)};
+        Assert.AreEqual(0, Fitness.TsgOgTerm(testNoEffect));
+
+        var testMissing = new List<(Gene, int)> { (MakeGene(ChrNo.chr1, 0.1), 1)};
+        Assert.AreEqual(-0.1, Fitness.TsgOgTerm(testMissing));
+
+        var testMissingTwice = new List<(Gene, int)> {(MakeGene(ChrNo.chr1, 0.1), 0)};
+        Assert.AreEqual(-0.2, Fitness.TsgOgTerm(testMissingTwice));
+
+        var testList = new List<(Gene, int)> {
+            (MakeGene(ChrNo.chr1, 0.1), 1), 
+            (MakeGene(ChrNo.chr1, 0.2), 0), 
+            (MakeGene(ChrNo.chr1, 0.3), 2)
+        };
+        Assert.AreEqual(-0.2 - 0.2 - 0.1, Fitness.TsgOgTerm(testList));
     }
     
     [Test]
     public void TestStressTerm()
     {
-        // TODO: @Felix
+        Assert.AreEqual(0, Fitness.StressTerm(46));
+        Assert.AreEqual(0, Fitness.StressTerm(45));
+        Assert.AreEqual(1, Fitness.StressTerm(47));
+        Assert.AreEqual(4, Fitness.StressTerm(48));
     }
     
     [Test]
