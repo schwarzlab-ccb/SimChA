f77d2f4543de823a4a1808a83c2c8d1af7cf81f3
Adam Streck <adam.streck@gmail.com>
2023-01-12 10:57:45 +0100

    Refactored Fitness calculation.

--- SimChA/Computation/Fitness.cs
+++ SimChA/Computation/Fitness.cs
@@ -8,57 +8,31 @@ namespace SimChA.Computation;
 
 public static class Fitness
 {
-    public static float Calculate(Karyotype karyotype, Dictionary<ChrNo, List<Gene>> essentialGenes,
-        Dictionary<ChrNo, List<Gene>> tsgOgGenes, SimParams simParams)
+    public static double Calculate(
+        Karyotype karyotype,
+        Dictionary<ChrNo, List<Gene>> essentialGenes,
+        Dictionary<ChrNo, List<Gene>> tsgOgGenes,
+        SimParams simParams)
     {
-        float stress = CalcStress(karyotype.ContigCount);
-        float tsgOgFitness = CalcTsgOgFitness(karyotype, tsgOgGenes);
-        float essentialityFitness = CalcEssentialityFitness(karyotype, essentialGenes);
-        return 1 - simParams.StressFraction * stress
-            + simParams.TsgOgFraction * tsgOgFitness
-            - simParams.EssentialFraction * essentialityFitness;
-    }
-
-    private static float CalcEssentialityFitness(Karyotype karyotype, Dictionary<ChrNo, List<Gene>> essentialGenes)
-    {
-        float essentialityFitness = 0;
-        Dictionary<Gene, int> essentialOccurences =FindGeneMultiplications(essentialGenes, 
-            karyotype.GetPresentGenes(essentialGenes));
-        foreach(var essentialGene in essentialOccurences)
-        {
-            essentialityFitness += Math.Max(1 - essentialGene.Value, 0) * essentialGene.Key.DeltaFitness;
-        }
-        return essentialityFitness;   
-    }
-
-    private static float CalcTsgOgFitness(Karyotype karyotype, Dictionary<ChrNo, List<Gene>> tsgOgGenes)
-    {
-        float tsgOgFitness = 0;
-        Dictionary<Gene, int> tsgOgOccurences = FindGeneMultiplications(tsgOgGenes, 
-            karyotype.GetPresentGenes(tsgOgGenes));
-        foreach(var tsgOgGenePresent in tsgOgOccurences)
-        {
-            tsgOgFitness += tsgOgGenePresent.Key.DeltaFitness * (tsgOgGenePresent.Value-2);
-        }
-        return tsgOgFitness;
-    }
-
-    private static Dictionary<Gene, int> FindGeneMultiplications(Dictionary<ChrNo, List<Gene>> geneLists, 
-    List<Gene> presentGenes)
-    {
-        Dictionary<Gene, int> geneListMultiplication = new Dictionary<Gene, int>();
-        foreach(var geneList in geneLists)
-        {
-            foreach(var gene in geneList.Value)
-            {
-                geneListMultiplication.Add(gene, presentGenes.Where(x => x == gene).Count());
-            }
-        }
-        return geneListMultiplication;
+        var tsgOgCNs = GeneCNs(karyotype.GetPresentGenes(tsgOgGenes));
+        var essCNs = GeneCNs(karyotype.GetPresentGenes(essentialGenes));
+        return 1 
+               - simParams.StressFraction * StressTerm(karyotype.ContigCount) 
+               + simParams.TsgOgFraction * TsgOgTerm(tsgOgCNs) 
+               - simParams.EssentialFraction * EssTerm(essCNs);
     }
 
     // Represents the limitation of space in the nucleus - more chromosomes ==> more stress
     // TODO: This needs to be validated
-    private static float CalcStress(int chrCount)
-        => (float)Math.Pow(Math.Max(0, chrCount - 46), 2);
+    public static double StressTerm(int contigCount)
+        => Math.Pow(Math.Max(0, contigCount - 46), 2);
+
+    public static double EssTerm(IEnumerable<(Gene gene, int CN)> essCNs)
+        => essCNs.Sum(g => Math.Max(1 - g.CN, 0) * g.gene.DeltaFitness);
+
+    public static double TsgOgTerm(IEnumerable<(Gene gene, int CN)> tsgOgCNs)
+        => tsgOgCNs.Sum(g => (g.CN - 2) * g.gene.DeltaFitness);
+
+    private static IEnumerable<(Gene, int)> GeneCNs(IEnumerable<Gene> presentGenes)
+        => presentGenes.GroupBy(g => g).Select(g => (g.Key, g.Count()));
 }
