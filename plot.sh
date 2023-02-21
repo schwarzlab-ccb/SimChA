7d5c212a53e355170b93898695eca9a8556d2dac
Tom Kaufmann <tkau93@gmail.com>
2023-02-21 15:39:37 +0100

    adjusted plot.sh to get input data from "out" folder not "in"

--- plot.sh
+++ plot.sh
@@ -1,5 +1,5 @@
 out="./out"$1
-in="./in"$1
+in="./out"$1
 
 
 echo "Selected data folder $out"
@@ -8,6 +8,7 @@ python3 scripts/consistent_segmentation.py -i $out/copynumbers.out -o $out/copyn
 
 echo "Plotting CN Tracks"
 # check if tree file exists
+echo "$in/clone_tree.new"
 if [ -f $in/clone_tree.new ]; then
     echo "Found .new tree, plotting it"
     python3 scripts/plot_copynumbers.py --input $out/copynumbers_consistent.out --tree $in/clone_tree.new --output-folder $out --type heatmap
