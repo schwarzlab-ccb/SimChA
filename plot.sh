9fd5da349653e7b03460feb5367472c364759bc8
Tom Kaufmann <tkau93@gmail.com>
2023-02-28 19:38:28 +0100

    use bash strict mode for plot.sh

--- plot.sh
+++ plot.sh
@@ -1,21 +1,22 @@
-out="./out"$1
-in="./out"$1
+#!/bin/bash
+set -euo pipefail
 
+dir="./out"
 
-echo "Selected data folder $out"
+echo "Selected data folder $dir"
 echo "Creating consistent segmentation"
-python3 scripts/consistent_segmentation.py -i $out/copynumbers.out -o $out/copynumbers_consistent.out
+python3 scripts/consistent_segmentation.py -i $dir/copynumbers.out -o $dir/copynumbers_consistent.out
 
 echo "Plotting CN Tracks"
 # check if tree file exists
-echo "$in/clone_tree.new"
-if [ -f $in/clone_tree.new ]; then
+echo "$dir/clone_tree.new"
+if [ -f $dir/clone_tree.new ]; then
     echo "Found .new tree, plotting it"
-    python3 scripts/plot_copynumbers.py --input $out/copynumbers_consistent.out --tree $in/clone_tree.new --output-folder $out --type heatmap
-elif  [ -f $in/clone_tree.dot ]; then
+    python3 scripts/plot_copynumbers.py --input $dir/copynumbers_consistent.out --tree $dir/clone_tree.new --output-folder $dir --type heatmap
+elif  [ -f $dir/clone_tree.dot ]; then
     echo "Found .dot tree, plotting it"
-    python3 scripts/plot_copynumbers.py --input $out/copynumbers_consistent.out --tree $in/clone_tree.dot --output-folder $out --type heatmap
+    python3 scripts/plot_copynumbers.py --input $dir/copynumbers_consistent.out --tree $dir/clone_tree.dot --output-folder $dir --type heatmap
 else
     echo "No tree found"
-    python3 scripts/plot_copynumbers.py --input $out/copynumbers_consistent.out --output-folder $out --type heatmap
+    python3 scripts/plot_copynumbers.py --input $dir/copynumbers_consistent.out --output-folder $dir --type heatmap
 fi
