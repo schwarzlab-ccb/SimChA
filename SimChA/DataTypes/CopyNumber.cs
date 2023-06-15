16b8038dae37f9514c78bfc85c07bf2464dc6b0b
Adam Streck <adam.streck@gmail.com>
2023-06-15 15:32:34 +0200

    Added NA to consistent CNs. The cmd option is now -s.

--- SimChA/DataTypes/CopyNumber.cs
+++ SimChA/DataTypes/CopyNumber.cs
@@ -3,5 +3,5 @@
 public record CopyNumber(GenRange Segment, int CNH1, int CNH2)
 {
     public string ToTSV()
-        => string.Join('\t', Segment.ChrNo, Segment.Start + 1, Segment.End, CNH1, CNH2);
+        => string.Join('\t', Segment.ChrNo, Segment.Start + 1, Segment.End, CNH1 >= 0 ? CNH1 : "NA", CNH2 >= 0 ? CNH2 : "NA");
 }
