2c3f3905e368f715c3981f34651b981ea6775f0b
Cody B Duncan <codybstrange93@gmail.com>
2023-07-21 10:20:22 +0200

    Added GenRange back in

---
+++ SimChA/DataTypes/GenRange.cs
@@ -0,0 +1,17 @@
+﻿namespace SimChA.DataTypes;
+
+public record GenRange(long Start, long End, string ChrNo)
+{
+    public long Length => End - Start;
+    
+    // True if this range is inside the other range
+    public bool IsInside(GenRange other) 
+        => Start >= other.Start && End <= other.End && ChrNo == other.ChrNo;
+
+    // True if this range shares at least one position with the other range
+    public bool Overlaps(GenRange other)
+        => Start < other.End && End > other.Start && ChrNo == other.ChrNo;
+    
+    public override string ToString() 
+        => $"{ChrNo}[{Start}:{End})";
+}
