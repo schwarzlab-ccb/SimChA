6c64015d71b63e6ed44d63e32a974b797c729695
Cody B Duncan <codybstrange93@gmail.com>
2023-10-17 12:06:16 +0200

    Fixed some more bugs in abc.py. Now works for number of event count estimation

--- scripts/abc.py
+++ scripts/abc.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import subprocess
+import numpy as np
 import os
 import argparse
 import platform
 import datetime as dt
 import json
-from pyabc import ABCSMC, RV, Distribution, settings
+import matplotlib.pyplot as plt
+from pyabc import ABCSMC, RV, Distribution, settings, visualization
 from pyabc.populationstrategy import ConstantPopulationSize
 
 settings.set_figure_params('pyabc')
@@ -23,7 +25,7 @@ def update_params_file(params):
     with open(file_path, 'r', encoding='utf-8') as json_file:
         configs = json.load(json_file)
 
-    configs["EventCount"] = params["n_events"]
+    configs["EventCount"] = int(params["n_events"])
 
     """configs["Signatures"]["CNs"]["Events"]["ChromDeletion"]["Prob"]        = params["w_chrom_del"]
     configs["Signatures"]["CNs"]["Events"]["ChromDuplication"]["Prob"]     = params["w_chrom_dup"]
@@ -34,11 +36,11 @@ def update_params_file(params):
     configs["Signatures"]["CNs"]["Events"]["BreakageFusionBridge"]["Prob"] = params["w_bfb"]
     configs["Signatures"]["CNs"]["Events"]["TailDeletion"]["Prob"]         = params["w_tail_del"]
     configs["Signatures"]["CNs"]["Events"]["WholeGenomeDoubling"]["Prob"]  = params["w_wgd"]
-    
-    configs["Signatures"]["CNs"]["Events"]["InternalInversion"]["Size"]   = params["l_int_inv"]*100_000
-    configs["Signatures"]["CNs"]["Events"]["InternalDeletion"]["Size"]    = params["l_int_del"]*100_000
-    configs["Signatures"]["CNs"]["Events"]["InternalDuplication"]["Size"] = params["l_int_dup"]*100_000
-    configs["Signatures"]["CNs"]["Events"]["InvertedDuplication"]["Size"] = params["l_inv_dup"]*100_000"""
+    # The length-scales have to be rounded to integer status 
+    configs["Signatures"]["CNs"]["Events"]["InternalInversion"]["Size"]   = int(params["l_int_inv"]*100_000)
+    configs["Signatures"]["CNs"]["Events"]["InternalDeletion"]["Size"]    = int(params["l_int_del"]*100_000)
+    configs["Signatures"]["CNs"]["Events"]["InternalDuplication"]["Size"] = int(params["l_int_dup"]*100_000)
+    configs["Signatures"]["CNs"]["Events"]["InvertedDuplication"]["Size"] = int(params["l_inv_dup"]*100_000)"""
     with open(file_path, 'w', encoding="utf-8") as json_file:
         json.dump(configs, json_file)
     # Return the path to the config file
@@ -48,8 +50,8 @@ def update_params_file(params):
 def run_simcha(params):
     param_file_path = update_params_file(params)
 
-    cmd = ["dotnet", "run", "--project",  "SimChA", "-C", f"{param_file_path}" "-R", "2000", "-O", f"abc_results", "--optimization", "-D", "data/hg19_1000", "-P", "pcawg_filtered_95_pc.tsv"]
-    output = subprocess.check_output(cmd, universal_newlines=True, shell=True)
+    cmd = f"dotnet run --project SimChA -C {param_file_path} -R 2000 -O abc_results --optimization -D data/hg19_1000 -P simcha_notebooks/data/pcawg_filtered_95_pc.tsv"
+    output = subprocess.check_output([cmd], universal_newlines=True, shell=True)
     # SimChA produces as its output the Euclidean sum of Wasserstein distances for each of the 
     # characteristic features of cancer genomes, printing the double to the command 
     last_line = output.strip().split("\n")[-1]
@@ -69,7 +71,11 @@ if __name__ == "__main__":
 
     # Uniform prior distributions for the various different properties of the simple events
     # We can also remove the number of events if we want
-    prior = Distribution(n_events    = RV("uniform", 30, 150))
+    event_count_values = np.arange(30,151)
+    event_count_prob   = [1/len(event_count_values)]*len(event_count_values)
+
+
+    prior = Distribution(n_events    = RV("rv_discrete", values=(event_count_values, event_count_prob)))
     """w_chrom_del = RV("uniform", 1, 10), 
             w_chrom_dup = RV("uniform", 1, 10), 
             w_int_dup   = RV("uniform", 1, 100),
@@ -99,7 +105,7 @@ if __name__ == "__main__":
     fig, ax = plt.subplots()
     for t in range(history.max_t + 1):
         df, w = history.get_distribution(t=t)
-        pyabc.visualization.plot_kde_1d(
+        visualization.plot_kde_1d(
             df,
             w,
             xmin=30,
