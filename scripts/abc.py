ff5490ce771ef39b2dbf87fa0f4311eb3082bfdc
Cody B Duncan <codybstrange93@gmail.com>
2023-10-17 10:21:28 +0200

    fixed a couple of bugs in abc script

--- scripts/abc.py
+++ scripts/abc.py
@@ -5,10 +5,10 @@ import argparse
 import platform
 import datetime as dt
 import json
-from pyabc import ABCSMC, RV, Distribution
+from pyabc import ABCSMC, RV, Distribution, settings
 from pyabc.populationstrategy import ConstantPopulationSize
 
-pyabc.settings.set_figure_params('pyabc')
+settings.set_figure_params('pyabc')
 
 def update_params_file(params):
     # Create the temporary parameter file
@@ -23,7 +23,7 @@ def update_params_file(params):
     with open(file_path, 'r', encoding='utf-8') as json_file:
         configs = json.load(json_file)
 
-    config["EventCount"] = params["n_events"]
+    configs["EventCount"] = params["n_events"]
 
     """configs["Signatures"]["CNs"]["Events"]["ChromDeletion"]["Prob"]        = params["w_chrom_del"]
     configs["Signatures"]["CNs"]["Events"]["ChromDuplication"]["Prob"]     = params["w_chrom_dup"]
@@ -69,9 +69,8 @@ if __name__ == "__main__":
 
     # Uniform prior distributions for the various different properties of the simple events
     # We can also remove the number of events if we want
-    prior = Distribution(
-            n_events    = RV("uniform", 30, 150)
-            """w_chrom_del = RV("uniform", 1, 10), 
+    prior = Distribution(n_events    = RV("uniform", 30, 150))
+    """w_chrom_del = RV("uniform", 1, 10), 
             w_chrom_dup = RV("uniform", 1, 10), 
             w_int_dup   = RV("uniform", 1, 100),
             w_int_del   = RV("uniform", 1, 100),
@@ -84,15 +83,15 @@ if __name__ == "__main__":
             l_int_dup   = RV("uniform", 1, 50),
             l_int_del   = RV("uniform", 1, 50),
             l_int_inv   = RV("uniform", 1, 50),
-            l_inv_dup   = RV("uniform", 1, 50)"""
-            )
+            l_inv_dup   = RV("uniform", 1, 50)
+            )"""
 
     # SimChA calculates the Euclidean-summed Wasserstein distance, so we don't need an observed distance
     observed_data = {"distance": 0.0}
 
     abc = ABCSMC(model, prior, distance_function=distance, population_size = 5)
     # ABC-SMC output is a SQL database
-    db_path = out_dir
+    db_path = f"{out_dir}/test.db"
     abc.new("sqlite:///"+db_path, observed_data)
 
     history = abc.run(minimum_epsilon = 0.1, max_nr_populations = 1)
