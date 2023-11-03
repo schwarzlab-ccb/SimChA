78f7fa85aaa127a13f6d3c024ccce4634956a189
Cody B Duncan <codybstrange93@gmail.com>
2023-11-03 11:53:01 +0100

    PCAWG samples get binned once at beginning of ABC sampling, and made the script more robust to path dependence

--- scripts/fitness_abc.py
+++ scripts/fitness_abc.py
@@ -43,11 +43,13 @@ def update_params_file(params):
     # Return the path to the config file
     return path
 
+def wrapper_for_simcha(params):
 
-def run_simcha(params):
+
+def run_simcha(params, genes_path, cohort_path, bootstrap_path, binned_path, repeats):
     param_file_path = update_params_file(params)
 
-    cmd = f"dotnet run --no-build --project SimChA -C {param_file_path}/fitted_params.json -R 250 -O {param_file_path}/out --optimization fitness -D data/hg19_1000 -M -B out/PCAWG/clones.tsv -P pcawg_filtered_95_pc.tsv"
+    cmd = f"dotnet run --no-build --project SimChA -C {param_file_path}/fitted_params.json -R {repeats} -O {param_file_path}/out --optimization fitness -D {genes_path} -M -B {bootstrap_path} -P {cohort_path} --binned-samples {binned_path}"
     output = subprocess.check_output([cmd], universal_newlines=True, shell=True)
     # SimChA produces as its output the Euclidean sum of Wasserstein distances for each of the 
     # characteristic features of cancer genomes, printing the double to the command 
@@ -64,6 +66,22 @@ def model(params):
 def distance(x,y):
     return abs(x["distance"] - y["distance"])
     
+def check_inputs(in_path, genes_path, cohort_path):
+
+    clones_file = "clones.tsv"
+    binned_file = "binned_CNs.tsv"
+
+    if !os.path.isdir(in_path):
+        subprocess.run([f"mkdir -p in_path"], shell=True)
+    # bootstrap file needed for sampling the fitnesses of the mcmc produces clones
+    if !os.path.isfile(os.path.join(in_path, clones_file)):
+        cmd = f"dotnet run --project SimChA -P {cohort_path} -O {in_path} -D {genes_path}"
+        subprocess.run([cmd], shell = True)
+
+    if !os.path.isfile(os.path.join(in_path, binned_file)):
+        cmd = f"dotnet run --project SimChA -P {cohort_path} -O {in_path} --bin-samples"
+        subprocess.run([cmd], shell = True)
+      
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="pyABC program to fit parameters in SimChA ")
@@ -71,7 +89,24 @@ if __name__ == "__main__":
     parser.add_argument('-n', "--name",type=str, default="results", help="Name for output directory to put SQL database produced by pyABC and the posterior plot produced")
     parser.add_argument("-c", "--nCPUs", type=int, default=16, help="Number of cpus to use")
     parser.add_argument("-w", "--weight", type=float, default=1, help="Total weight associated with the initial guess for the fitness parameters. Higher weight means lower variance, i.e. sample closer to initial guesses.")
+    parser.add_argument("-r", "--repeats", type=int, default=250, help="Number of samples to generate for each SimChA run")
     args = parser.parse_args()
+
+    
+    # Initialize the input required for the fitness sampling.
+    # These inputs are run-agnostic, so we just set it up once at the beginning
+    cohort_path = "data/pcawg_filtered_95_pc.tsv"
+    genes_path = "data/hg19_1000"
+    inputs_path = "fitness_in"
+    check_inputs(inputs_path, genes_path, cohort_path)
+    
+    bootstrap_path = os.path.join(inputs_path, "clones.tsv")
+    binned_path    = os.path.join(inputs_path, "binned_CNs.tsv")
+
+    # We use a wrapper function so that we can run SimChA with the relevant inputs and we only need to change them here
+    def model_wrapper(params):
+        return run_simcha(params, genes_path, cohort_path, bootstrap_path, binned_path, args.repeats())
+
 
 
     pwd = os.getcwd()
@@ -89,7 +124,7 @@ if __name__ == "__main__":
     # SimChA calculates the distance between simulated and observation, so we don't need an observed distance
     observed_data = {"distance": 0.0}
     sampler = sampler.MulticoreEvalParallelSampler(n_procs=args.nCPUs())
-    abc = ABCSMC(model, prior, distance_function=distance, population_size = 100, sampler = sampler)
+    abc = ABCSMC(model_wrapper, prior, distance_function=distance, population_size = 100, sampler = sampler)
     # ABC-SMC output is a SQL database
     db_path = f"{out_dir}/test.db"
     abc.new("sqlite:///"+db_path, observed_data)
