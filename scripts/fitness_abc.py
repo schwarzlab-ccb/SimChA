7b3075716e571df16411a7bcff78c56cd0cb0ca6
Cody B Duncan <codybstrange93@gmail.com>
2023-12-01 16:59:37 +0100

    updated the fitness abc sampling script to be more in line with the updated events ABC script

--- scripts/fitness_abc.py
+++ scripts/fitness_abc.py
@@ -5,11 +5,19 @@ import os
 import argparse
 import platform
 import datetime as dt
+import uuid
 import json
 import matplotlib.pyplot as plt
 from pyabc import ABCSMC, RV, Distribution, settings, visualization, sampler
+from pyabc.visualization import plot_kde_matrix
 from pyabc.populationstrategy import ConstantPopulationSize
 import pyabc
+
+######################
+#
+# This python program perfoms the pyABC sampling for the fitness parameters used in SimChA
+#
+######################
 
 ######
 # Files needed to infer the fitness parameters:
@@ -21,10 +29,8 @@ import pyabc
 settings.set_figure_params('pyabc')
 
 def update_params_file(params):
-    # Create the temporary parameter file
-    
-    foldername = f"{int(dt.datetime.now().timestamp())}_"+"_".join(str(p) for p in params.values())
-    
+    # Create the temporary parameter folder and file
+    foldername = f"{uuid.uuid4()}"
     path = f"{pwd}/temp/{foldername}"
     subprocess.run([f"mkdir -p {path}"], shell = True)
     subprocess.run([f"cp fitted_params.json {path}"], shell = True)
@@ -47,7 +53,7 @@ def update_params_file(params):
 def run_simcha(params, genes_path, cohort_path, bootstrap_path, binned_path, repeats):
     param_file_path = update_params_file(params)
 
-    cmd = f"dotnet run --no-build --project SimChA -C {param_file_path}/fitted_params.json -R {repeats} -O {param_file_path}/out --optimization fitness -D {genes_path} -M -B {bootstrap_path} -P {cohort_path} --binned-samples {binned_path}"
+    cmd = f"dotnet run --no-build --project SimChA -C {param_file_path}/fitted_params.json -R {repeats} -O {param_file_path}/out --optimization fitness -D {genes_path} -M -B {bootstrap_path} -P {cohort_path} --binned-samples {binned_path} --autosomes-only"
     output = subprocess.check_output([cmd], universal_newlines=True, shell=True)
     # SimChA produces as its output the Euclidean sum of Wasserstein distances for each of the 
     # characteristic features of cancer genomes, printing the double to the command 
@@ -70,23 +76,28 @@ def check_inputs(in_path, genes_path, cohort_path):
         subprocess.run([f"mkdir -p in_path"], shell=True)
     # bootstrap file needed for sampling the fitnesses of the mcmc produces clones
     if !os.path.isfile(os.path.join(in_path, clones_file)):
-        cmd = f"dotnet run --project SimChA -P {cohort_path} -O {in_path} -D {genes_path}"
+        cmd = f"dotnet run --no-build --project SimChA -P {cohort_path} -O {in_path} -D {genes_path}"
         subprocess.run([cmd], shell = True)
 
     if !os.path.isfile(os.path.join(in_path, binned_file)):
-        cmd = f"dotnet run --project SimChA -P {cohort_path} -O {in_path} --bin-samples"
+        cmd = f"dotnet run --no-build --project SimChA -P {cohort_path} -O {in_path} --bin-samples"
         subprocess.run([cmd], shell = True)
       
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="pyABC program to fit parameters in SimChA ")
     #parser.add_argument('-R', "--repeats", type=int, default=500, help="Number of SimChA simulated samples to generate for each pyABC sample")
-    parser.add_argument('-n', "--name",type=str, default="results", help="Name for output directory to put SQL database produced by pyABC and the posterior plot produced")
-    parser.add_argument("-c", "--nCPUs", type=int, default=16, help="Number of cpus to use")
+    parser.add_argument('-n', "--name",type=str, default="fitness_abc_results", help="Name for output directory to put SQL database produced by pyABC and the posterior plot produced")
     parser.add_argument("-w", "--weight", type=float, default=1, help="Total weight associated with the initial guess for the fitness parameters. Higher weight means lower variance, i.e. sample closer to initial guesses.")
     parser.add_argument("-r", "--repeats", type=int, default=250, help="Number of samples to generate for each SimChA run")
+    parser.add_argument('--n_procs', type=int, default = 8, help="Number of parallel threads that pyABC will use")
+    parser.add_argument("--n_pop", type=int, default=150, help="Population size, i.e. number of accepted samples (particles) to move on to new generation")
+    parser.add_argument("--max_gen", type=int, default=10, help="Maximum number of generations to consider")
+    parser.add_argument("--min_eps", type=float, default=0.01, help="Minimum acceptance threshold before ending the ABC Sampling prematurely")
     args = parser.parse_args()
 
+    # Build the program once at the beginning
+    subprocess.run(["dotnet build SimChA"])
     
     # Initialize the input required for the fitness sampling.
     # These inputs are run-agnostic, so we just set it up once at the beginning
@@ -94,7 +105,7 @@ if __name__ == "__main__":
     genes_path = "data/hg19_1000"
     inputs_path = "fitness_in"
     check_inputs(inputs_path, genes_path, cohort_path)
-    
+
     bootstrap_path = os.path.join(inputs_path, "clones.tsv")
     binned_path    = os.path.join(inputs_path, "binned_CNs.tsv")
 
@@ -104,7 +115,7 @@ if __name__ == "__main__":
 
 
     pwd = os.getcwd()
-    out_dir = "fitness_abc_"+args.name
+    out_dir = args.name
     subprocess.run([f"mkdir -p {out_dir}"], shell=True)
 
     # Using symmetric concentration parameters for the Dirichlet distribution.
@@ -118,15 +129,22 @@ if __name__ == "__main__":
 
     # SimChA calculates the distance between simulated and observation, so we don't need an observed distance
     observed_data = {"distance": 0.0}
-    sampler = sampler.MulticoreEvalParallelSampler(n_procs=args.nCPUs)
-    abc = ABCSMC(model_wrapper, prior, distance_function=distance, population_size = 100, sampler = sampler)
+    sampler = sampler.MulticoreEvalParallelSampler(n_procs=args.n_procs)
+    abc = ABCSMC(model_wrapper, prior, distance_function=distance, population_size = args.n_pop, sampler = sampler)
     # ABC-SMC output is a SQL database
-    db_path = f"{out_dir}/test.db"
+    db_path = f"{out_dir}/parameters.db"
     abc.new("sqlite:///"+db_path, observed_data)
 
-    history = abc.run(minimum_epsilon = 0.01, max_nr_populations = 10)
+    # Main part of the ABC Sampling
+    history = abc.run(minimum_epsilon = args.min_eps, max_nr_populations = args.max_gen)
 
-    fig, ax = plt.subplots()
+    # Visualization of the posteriors of the fitness parameters
+    df, w = history.get_distribution(m=0, t=history.max_t)
+    arr_ax = plot_kde_matrix(df, w)
+    fig = arr_ax[0,0].get_figure()
+    fig.savefig(f"{out_dir}/kde_matrix.png", dpi=300, bbox_inches="tight")
+
+    """fig, ax = plt.subplots()
     for t in range(history.max_t + 1):
         df, w = history.get_distribution(t=t)
         visualization.plot_kde_1d(
@@ -140,6 +158,6 @@ if __name__ == "__main__":
             label=f"PDF t={t}",
         )
     ax.legend()
-    plt.savefig(f"{out_dir}/posterior_generations.png")
+    plt.savefig(f"{out_dir}/posterior_generations.png")"""
 
 
