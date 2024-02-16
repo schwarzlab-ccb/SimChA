5036ca00d74e15e35911e563064b580c98006668
Cody B Duncan (CHEOPS) <codybstrange93@gmail.com>
2024-02-16 15:26:51 +0100

    updated the simcha optimization script to be path-in-independent

--- scripts/simcha_optimize.py
+++ scripts/simcha_optimize.py
@@ -8,23 +8,24 @@ from datetime import datetime
 import uuid
 import json
 
-def generate_cohort(param_file, genes_path, out_path, repeats, all_chromomsomes):
-    cmd = f"dotnet run --no-build --project SimChA -C {param_file} -R {repeats} -O {out_path} -D {genes_path}"
+def generate_cohort(path, param_file, genes_path, out_path, repeats, all_chromomsomes):
+    cmd = f"dotnet run --no-build --project {path} -C {param_file} -R {repeats} -O {out_path} -D {genes_path}"
     if not all_chromosomes:
         cmd += " --autosomes-only"
     subprocess.run([cmd], shell=True)
     return
    
-def run_simcha(param_file, genes_path, cohort_path, repeats, all_chromosomes, out_dir):
+def run_simcha(path, param_file, genes_path, cohort_path, repeats, all_chromosomes, out_dir):
     # Run command for SimChA
-    cmd = f"dotnet run --no-build --project SimChA -C {param_file} -R {repeats} -O {out_dir} --optimization -D {genes_path} -P {cohort_path}"
+    cmd = f"dotnet run --no-build --project {path} -C {param_file} -R {repeats} -O {out_dir} --optimization -D {genes_path} -P {cohort_path}"
     if not all_chromosomes:
         cmd += " --autosomes-only"
     subprocess.run([cmd], shell=True)
    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="pyABC program to fit parameters in SimChA ")
-    parser.add_argument('-N', "--name",type=str, default="optimization_results", help="Name for output directory to put SQL database produced by pyABC and the posterior plot produced")
+    parser.add_argument("-P", "--path", type=str, default="SimChA", help="Path to SimChA")
+    parser.add_argument('-N', "--name", type=str, default="optimization_results", help="Name for output directory to put SQL database produced by pyABC and the posterior plot produced")
     parser.add_argument("-R", "--repeats", type=int, default=2000, help="Number of samples/repeats to generate for each SimChA run")
     parser.add_argument('--n_procs', type=int, default = 8, help="Number of parallel threads that pyABC will use")
     parser.add_argument("--n_pop", type=int, default=150, help="Population size, i.e. number of accepted samples (particles) to move on to new generation")
@@ -37,14 +38,14 @@ if __name__ == "__main__":
 
     genes_path = args.genes_path
     # Build the program once for the corresponding thread, in case any changes have been made to SimChA
-    subprocess.run(["dotnet build SimChA"], shell = True)
+    subprocess.run([f"dotnet build {args.path}"], shell = True)
 
     # If we use SimChA-generated data as the ground-truth, we first have to generate that data
     param_file = args.config_file
     if args.test:
         cohort_dir_path = "out/ground_truth"
         # Generate the simulated data
-        generate_cohort(param_file, genes_path, cohort_dir_path, args.repeats, args.all_chromosomes)
+        generate_cohort(args.path, param_file, genes_path, cohort_dir_path, args.repeats, args.all_chromosomes)
         cohort_path = "out/ground_truth/copynumbers.tsv"
     else:
         cohort_path = args.data_path
@@ -53,4 +54,4 @@ if __name__ == "__main__":
     out_dir = args.name
     subprocess.run([f"mkdir -p {out_dir}"], shell=True)
 
-    run_simcha( param_file, genes_path, cohort_path, args.repeats, args.all_chromosomes, out_dir)
+    run_simcha(args.path, param_file, genes_path, cohort_path, args.repeats, args.all_chromosomes, out_dir)
