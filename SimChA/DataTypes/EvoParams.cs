c339eaa913dce20ce517e79e8ea4715ddaa1e8d1
Cody B Duncan <codybstrange93@gmail.com>
2025-02-05 15:37:37 +0100

    Reintroduce time-based evolution into Evolver with minimal code

--- SimChA/DataTypes/EvoParams.cs
+++ SimChA/DataTypes/EvoParams.cs
@@ -6,5 +6,8 @@ public record EvoParams(
     bool WithFitness, // Evolve with or without fitness
     int MaxTries, // Maximum number of tries to generate a new set of events in a time step
     double EventCost, // Flag to turn on and off event parameters being weighted by ploidy
-    bool TetraploidStart // Flag to start the simulation from a tetraploid state
+    bool TetraploidStart, // Flag to start the simulation from a tetraploid state
+    bool EvolveInTime, // Flag to evolve in time
+    float MaxTime, // Maximum time to evolve
+    float MutationRate // Average number of mutations per unit time
 );
