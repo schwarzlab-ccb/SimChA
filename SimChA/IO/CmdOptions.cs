9bb8538fac90161e82196afe161f5eb2e7d5f987
Adam Streck <astreck2@uni-koeln.de>
2026-03-10 17:22:10 +0100

    Readme and config update.

--- SimChA/IO/CmdOptions.cs
+++ SimChA/IO/CmdOptions.cs
@@ -7,7 +7,7 @@ public class CmdOptions
     [Option('O', "output", Required = false, Default = "./out", HelpText = "The path to the output files.")]
     public string OutputPath { get; set; }
 
-    [Option('C', "config", Required = false, Default = "./simcha_config.json", HelpText = "A json file with configuration of the experiment.")]
+    [Option('C', "config", Required = false, Default = "./configs/main_config.json", HelpText = "A json file with configuration of the experiment.")]
     public string ConfigFile { get; set; }
 
     [Option('T', "tree", Required = false, Default = "", HelpText = "A clone file that describes the tree to be built.")]
