425217afb3f549c1a1cd23339f17c1b618b7b497
Adam Streck <adam.streck@iccb-cologne.org>
2026-03-26 11:02:48 +0100

    Explicitly counting raw fitness components.

--- SimChA/Data/SampleStat.cs
+++ SimChA/Data/SampleStat.cs
@@ -13,6 +13,10 @@ public record SampleStat(
     double Tsg,
     double Og,
     double Ess,
+    double StressRaw,
+    double TsgRaw,
+    double OgRaw,
+    double EssRaw,
     int MutCount,
     string Mixture,
     int NumRejectedEvents)
@@ -28,6 +32,10 @@ public record SampleStat(
            "\ttsg" +
            "\tog" +
            "\tess" +
+           "\tstress_raw" +
+           "\ttsg_raw" +
+           "\tog_raw" +
+           "\tess_raw" +
            "\tdist" +
            "\tmixture"+
            "\tn_rejected_events";
@@ -43,6 +51,10 @@ public record SampleStat(
         $"\t{Tsg}" +
         $"\t{Og}" +
         $"\t{Ess}" +
+        $"\t{StressRaw}" +
+        $"\t{TsgRaw}" +
+        $"\t{OgRaw}" +
+        $"\t{EssRaw}" +
         $"\t{MutCount}" +
         $"\t{Mixture}"+
         $"\t{NumRejectedEvents}";
@@ -55,18 +67,21 @@ public record SampleStat(
         var kar = sample.Karyotype;
 
         double ploidy = CalcPloidy(kar, refGen);
-        double stress = Fitness.StressTerm(refGen.SexGenomeLen[(int) kar.Sex], kar.GenomeLen());
+        double stress_raw = Fitness.StressTerm(refGen.SexGenomeLen[(int) kar.Sex], kar.GenomeLen());
         var geneData = refGen.SexGeneLists[(int) kar.Sex];
-        double tsg = Fitness.TsgOgTerm(geneData[(int) GeneLT.TSG], kar.GeneCounts[(int) GeneLT.TSG]);
-        double og = Fitness.TsgOgTerm(geneData[(int) GeneLT.OG], kar.GeneCounts[(int) GeneLT.OG]);
-        double ess = Fitness.EssTerm(geneData[(int) GeneLT.Ess], kar.GeneCounts[(int) GeneLT.Ess]);
-        double fitnessVal = 1 + stress * fParams.Stress + (og - tsg) * fParams.TsgOg + ess * fParams.Essentiality;
+        double tsg_raw = Fitness.TsgOgTerm(geneData[(int) GeneLT.TSG], kar.GeneCounts[(int) GeneLT.TSG]) ;
+        double og_raw = Fitness.TsgOgTerm(geneData[(int) GeneLT.OG], kar.GeneCounts[(int) GeneLT.OG]);
+        double ess_raw = Fitness.EssTerm(geneData[(int) GeneLT.Ess], kar.GeneCounts[(int) GeneLT.Ess]);
+        double fitnessVal = 1 + stress_raw * fParams.Stress + (og_raw - tsg_raw) * fParams.TsgOg + ess_raw * fParams.Essentiality;
         
         int nRejectedEvents = sample.Events.Sum(e => e.NumRejections);
         double fitnessTarget = double.IsNaN(sample.FitnessTarget) ? kar.FitnessVal : sample.FitnessTarget;
 
-        var res = new SampleStat(sample.SampleId, sample.ParentId, kar.Sex, ploidy, fitnessVal, 
-            fitnessTarget, stress, tsg, og, ess, 
+        var res = new SampleStat(
+            sample.SampleId, sample.ParentId, kar.Sex, ploidy, 
+            fitnessVal, fitnessTarget, 
+            stress_raw* fParams.Stress, tsg_raw * fParams.TsgOg, og_raw * fParams.TsgOg, ess_raw * fParams.Essentiality,
+            stress_raw, tsg_raw, og_raw, ess_raw, 
             sample.Events.Count,  sample.MixtureString(), nRejectedEvents);
         return res;
     }
